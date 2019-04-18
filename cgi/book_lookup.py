82dcc3eab8fcf640a0e7d1d1b976d80a81ae3e16
John David Pressman <jd@jdpressman.com>
2019-04-17 19:01:57 -0700

    Update CSS and HTML to be less ghastly

--- cgi/book_lookup.py
+++ cgi/book_lookup.py
@@ -9,6 +9,8 @@ import codecs
 import sqlite3
 
 import cgi
+import cgitb
+cgitb.enable()
 
 def main():
     writer = codecs.getwriter('utf-8')(sys.stdout.buffer)
@@ -20,18 +22,68 @@ def main():
         "books.sqlite")
     books_db = sqlite3.connect(books_db_path)
     query_cursor = books_db.cursor()
-    query_cursor.execute("select title, author, pubtext, publisher, edition, awards, "
+    query_cursor.execute("select title, author, pubtext, edition, awards, "
                          "summary, subjects, dewey, locc, isbn10, isbn13, notes, availability "
                          "from books where key=?", (book_id,))
     book_data = list(query_cursor.fetchall()[0])
-    # Swap all empty columns for a not available marker.
-    for column in enumerate(book_data):
-        if column[1] == '':
-            book_data[column[0]] = 'N.A'
-        else:
-            pass
+    
     # Construct HTML.
-    book_data_html = ["<p>" + str(column) + "</p>\n" for column in book_data]
+    book_data_html = ["<p id='author'>" + "by "
+                      + ' '.join(reversed(str(book_data[1]).split(","))).strip()
+                      + "</p>",
+                      "<p id='pubtext'>" + "Published: " +
+                      str(book_data[2]) + ' ' + str(book_data[3]) + "</p>"]
+    if book_data[4]:
+        book_data_html.append(
+            "<ul id='awards'>" +
+            ''.join(["<li>" + award + "</li>" for award in book_data[4].split()])
+            + "</ul>")
+    if book_data[5]:
+        book_data_html.append("<p id='summary'>" + str(book_data[5]) + "</p>")
+    else:
+        book_data_html.append("<p id='summary'>" + "No summary is available for " +
+                              book_data[0].title() + ".</p>")
+    book_data_html.append("<div id='subject-div'>")
+    book_data_html.append("<h2>" + "Subjects" + "</h2>")
+    book_data_html.append(
+        "<ul id='subjects'>" +
+        ''.join(["<li>" +
+                 ' '.join([word.capitalize() for word in subject.split("_")])
+                 + "</li>" for subject in str(book_data[6]).split(",")])
+        + "</ul>")
+    book_data_html.append("</div>")
+    book_data_html.append("<div id='id-numbers'>")
+    book_data_html.append("<h2>" + "Identifiers" + "</h2>")
+    if book_data[7]:
+        book_data_html.append("<p id='dewey'>" +
+                              "<b class='field'>Dewey Decimal: </b>" +
+                              str(book_data[7]) +
+                              "</p>")
+    if book_data[8]:
+        book_data_html.append("<p id='locc'>" +
+                              "<b class='field'>Library of Congress #: </b>" +
+                              str(book_data[8]) +
+                              "</p>")
+    if book_data[9]:
+        book_data_html.append("<p id='isbn10'>" +
+                              "<b class='field'>IBSN10: </b>" +
+                              str(book_data[9]) +
+                              "</p>")
+    if book_data[10]:
+        book_data_html.append("<p id='isbn13'>" +
+                              "<b class='field'>ISBN13: </b>" +
+                              str(book_data[10]) +
+                              "</p>")
+    book_data_html.append("</div>")
+    if book_data[12] == 1:
+        book_data_html.append("<p id='availability'>" + "<b>Availability: </b>" + 
+                              "<span class='available'>" + str(book_data[12]) + "</span>"
+                              + " copy available</p>")
+    else:
+        book_data_html.append("<p id='availability'>" + + "<b>Availability: </b>" + 
+                              "<span class='unavailable'>" + str(book_data[12]) + "</span>"
+                              + " copy available</p>")
+
     html_start_block = ("<html>\n"
                         "  <head>\n"
                         '    <link href="/css/lookup.css" rel="stylesheet">\n'
@@ -40,7 +92,7 @@ def main():
                         '  </head>\n'
                         '  <body>\n'
                         '    <article>\n')
-    book_data_html[0] = '<h1> ' + book_data_html[0] + ' </h1>'
+    book_data_html.insert(0, '<h1> ' + str(book_data[0]).title() + ' </h1>')
     html_start_block += ''.join(book_data_html)
     html_start_block += ('    </article>\n'
                          '  </body>\n'
